c9671f92f7b09ccfb4521cef68230105501ce1e8
PavelShestun <pavelshestun@yandex.ru>
2025-04-28 22:09:16 +0300

    Fix PDB files and update io.f90 to correctly parse CA atoms

--- src/io.f90
+++ src/io.f90
@@ -5,51 +5,48 @@ contains
     subroutine read_pdb(filename, structure)
         character(len=*), intent(in) :: filename
         type(ProteinStructure), intent(out) :: structure
-        integer :: unit, io_status, i, num_residues, num_atoms
+        integer :: i, io_status, num_residues
         character(len=80) :: line
         real(8) :: x, y, z
-        character(len=4) :: prev_residue_num
 
-        ! Подсчет числа остатков и атомов
+        ! Инициализация структуры
+        structure%num_residues = 0
+        structure%num_atoms = 0
+
+        ! Открываем файл
+        open(unit=10, file=filename, status='old', action='read', iostat=io_status)
+        if (io_status /= 0) then
+            print *, "Ошибка при открытии файла: ", filename
+            stop
+        end if
+
+        ! Подсчитываем количество Cα-атомов
         num_residues = 0
-        num_atoms = 0
-        open(newunit=unit, file=filename, status='old', action='read', iostat=io_status)
-        if (io_status /= 0) stop "Ошибка чтения PDB файла"
-        prev_residue_num = '    '
         do
-            read(unit, '(A)', iostat=io_status) line
+            read(10, '(A)', iostat=io_status) line
             if (io_status /= 0) exit
-            if (line(1:4) == 'ATOM' .and. trim(line(13:16)) == 'CA') then
-                num_atoms = num_atoms + 1
-                if (num_atoms == 1) then
-                    num_residues = 1
-                    prev_residue_num = line(23:26)
-                else if (line(23:26) /= prev_residue_num) then
-                    num_residues = num_residues + 1
-                    prev_residue_num = line(23:26)
-                end if
+            if (line(1:4) == 'ATOM' .and. line(13:16) == ' CA ') then
+                num_residues = num_residues + 1
             end if
         end do
-        close(unit)
+        rewind(10)
 
-        ! Выделение памяти
+        ! Выделяем память
         structure%num_residues = num_residues
-        structure%num_atoms = num_atoms
+        structure%num_atoms = num_residues  ! У нас только Cα-атомы
         allocate(structure%residues(num_residues))
         do i = 1, num_residues
-            allocate(structure%residues(i)%atoms(1))  ! Только CA
-            structure%residues(i)%name = 'ALA'  ! Упрощение
-            structure%residues(i)%atoms(1)%name = 'CA'
-            structure%residues(i)%atoms(1)%mass = 1.0d0
+            allocate(structure%residues(i)%atoms(1))  ! Только один атом (Cα) на остаток
+            structure%residues(i)%atoms(1)%mass = 12.0d0  ! Масса углерода
         end do
+        allocate(structure%internal_coords(2 * num_residues))
 
-        ! Чтение координат
-        open(newunit=unit, file=filename, status='old', action='read')
+        ! Читаем координаты
         i = 0
         do
-            read(unit, '(A)', iostat=io_status) line
+            read(10, '(A)', iostat=io_status) line
             if (io_status /= 0) exit
-            if (line(1:4) == 'ATOM' .and. trim(line(13:16)) == 'CA') then
+            if (line(1:4) == 'ATOM' .and. line(13:16) == ' CA ') then
                 i = i + 1
                 read(line(31:38), *) x
                 read(line(39:46), *) y
@@ -57,30 +54,36 @@ contains
                 structure%residues(i)%atoms(1)%x = x
                 structure%residues(i)%atoms(1)%y = y
                 structure%residues(i)%atoms(1)%z = z
+                ! Устанавливаем начальные внутренние координаты (заглушка)
+                structure%internal_coords(2*i-1) = 0.0d0  ! phi
+                structure%internal_coords(2*i) = 0.0d0    ! psi
             end if
         end do
-        close(unit)
 
-        ! Упрощенное определение внутренних координат (заглушка)
-        allocate(structure%internal_coords(2 * num_residues))
-        structure%internal_coords = -60.0d0  ! Пример: phi = -60, psi = -60
+        close(10)
     end subroutine read_pdb
 
     subroutine write_pdb(filename, structure)
         character(len=*), intent(in) :: filename
         type(ProteinStructure), intent(in) :: structure
-        integer :: unit, i, atom_idx
-        open(newunit=unit, file=filename, status='replace', action='write')
-        atom_idx = 0
+        integer :: i, io_status
+
+        open(unit=11, file=filename, status='replace', action='write', iostat=io_status)
+        if (io_status /= 0) then
+            print *, "Ошибка при открытии файла для записи: ", filename
+            stop
+        end if
+
         do i = 1, structure%num_residues
-            atom_idx = atom_idx + 1
-            write(unit, '(A6,I5,1X,A4,1X,A3,1X,I4,4X,3F8.3)') &
-                'ATOM  ', atom_idx, 'CA  ', 'ALA', i, &
+            write(11, '(A6,I5,1X,A4,1X,A3,1X,I4,4X,3F8.3,2F6.2,10X,A1)') &
+                'ATOM  ', i, 'CA  ', 'ALA', i, &
                 structure%residues(i)%atoms(1)%x, &
                 structure%residues(i)%atoms(1)%y, &
-                structure%residues(i)%atoms(1)%z
+                structure%residues(i)%atoms(1)%z, &
+                1.00, 0.00, 'C'
         end do
-        write(unit, '(A)') 'END'
-        close(unit)
+        write(11, '(A)') 'END'
+
+        close(11)
     end subroutine write_pdb
 end module io
